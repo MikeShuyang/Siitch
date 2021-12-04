bc0592eda93aa36bde8ac38bbeb05e1d6660c43d
Ben Mathews <bsmathew@icloud.com>
2021-12-04 15:57:54 -0800

    fixed green border overlapping text

--- Comparing/ComparePage.js
+++ Comparing/ComparePage.js
@@ -574,8 +574,10 @@ export const comparePage = ({route}) => {
                                 /> }
                                 <Text style={selectedcategory1.localeCompare('Time to decompose') != 0 && styles.boldTextFormatBlueCompare || styles.boldTextFormatRedCompare}>{numberWithCommas(parseInt(f1[selectedcategory1])*prod1Total)} {f1[selectedmetrictodisplay]}</Text>
                             </View>
-                            <Text style={styles.textFormatCompare}>{prod1Total == 1 ? f1[selectedmeasurement] : "total"}</Text>
-                            <Text style={styles.textFormatCompare}>{f1[selectedsize]}</Text>
+                            <View>
+                                <Text style={styles.textFormatCompare}>{prod1Total == 1 ? f1[selectedmeasurement] : "total"}</Text>
+                                <Text style={styles.textFormatCompare, {paddingBottom: 5}}>{f1[selectedsize]}</Text>
+                            </View>
                         </View>
                         <View center style={getTextStyle(parseInt(f2[selectedcategory2]*prod2Total), getMinValue())}>
 
@@ -600,7 +602,7 @@ export const comparePage = ({route}) => {
                                 <Text style={selectedcategory2.localeCompare('Time to decompose') != 0 && styles.boldTextFormatBlueCompare || styles.boldTextFormatRedCompare}>{numberWithCommas(parseInt(f2[selectedcategory2])*prod2Total)} {f2[selectedmetrictodisplay]}</Text>
                             </View>
                             <Text style={styles.textFormatCompare}>{prod2Total == 1 ? f2[selectedmeasurement] : "total"}</Text>
-                            <Text style={styles.textFormatCompare}>{f2[selectedsize]}</Text>
+                            <Text style={styles.textFormatCompare, {paddingBottom: 5}}>{f2[selectedsize]}</Text>
                         </View>
                     </View>
                     {
@@ -631,7 +633,7 @@ export const comparePage = ({route}) => {
                                         <Text style={selectedcategory3.localeCompare('Time to decompose') != 0 && styles.boldTextFormatBlueCompare || styles.boldTextFormatRedCompare}>{numberWithCommas(parseInt(f3[selectedcategory3])*prod3Total)} {f3[selectedmetrictodisplay]}</Text>
                                     </View>
                                     <Text style={styles.textFormatCompare}>{prod3Total == 1 ? f3[selectedmeasurement] : "total"}</Text>
-                                    <Text style={styles.textFormatCompare}>{f3[selectedsize]}</Text>
+                                    <Text style={styles.textFormatCompare, {paddingBottom: 5}}>{f3[selectedsize]}</Text>
                                 </View>
                             }
                             {
@@ -661,7 +663,7 @@ export const comparePage = ({route}) => {
                                         <Text style={selectedcategory4.localeCompare('Time to decompose') != 0 && styles.boldTextFormatBlueCompare || styles.boldTextFormatRedCompare}>{numberWithCommas(parseInt(f4[selectedcategory4])*prod4Total)} {f4[selectedmetrictodisplay]}</Text>
                                     </View>
                                     <Text style={styles.textFormatCompare}>{prod4Total == 1 ? f4[selectedmeasurement] : "total"}</Text>
-                                    <Text style={styles.textFormatCompare}>{f4[selectedsize]}</Text>
+                                    <Text style={styles.textFormatCompare, {paddingBottom: 5}}>{f4[selectedsize]}</Text>
                                 </View>
                             }
                         </View>
@@ -694,7 +696,7 @@ export const comparePage = ({route}) => {
                                         <Text style={selectedcategory5.localeCompare('Time to decompose') != 0 && styles.boldTextFormatBlueCompare || styles.boldTextFormatRedCompare}>{numberWithCommas(parseInt(f5[selectedcategory5])*prod5Total)} {f6[selectedmetrictodisplay]}</Text>
                                     </View>
                                     <Text style={styles.textFormatCompare}>{prod5Total == 1 ? f5[selectedmeasurement] : "total"}</Text>
-                                    <Text style={styles.textFormatCompare}>{f5[selectedsize]}</Text>
+                                    <Text style={styles.textFormatCompare, {paddingBottom: 5}}>{f5[selectedsize]}</Text>
                                 </View>
                             }
                             {
@@ -724,7 +726,7 @@ export const comparePage = ({route}) => {
                                         <Text style={selectedcategory6.localeCompare('Time to decompose') != 0 && styles.boldTextFormatBlueCompare || styles.boldTextFormatRedCompare}>{numberWithCommas(parseInt(f6[selectedcategory6])*prod6Total)} {f6[selectedmetrictodisplay]}</Text>
                                     </View>
                                     <Text style={styles.textFormatCompare}>{prod6Total == 1 ? f6[selectedmeasurement] : "total"}</Text>
-                                    <Text style={styles.textFormatCompare}>{f6[selectedsize]}</Text>
+                                    <Text style={styles.textFormatCompare, {paddingBottom: 5}}>{f6[selectedsize]}</Text>
                                 </View>
                             }
                         </View>
@@ -916,20 +918,22 @@ function getTextStyle(val1, val2) {
     if(val1 === val2) {
         return {
             flex: 1,
+            flexDirection: "column",
             borderColor: '#6dbd64',
             borderRadius: 40,
             borderWidth: 5,
             width: DeviceWidth*0.3,
-            height: DeviceWidth*0.85,
+            //height: DeviceWidth*0.85,
             alignItems:'center',
             transform: [{scale: 0.8}],
-            justifyContent: 'center'
+            justifyContent: 'center',
         }
     } else {
         return {
             flex: 1,
+            flexDirection: "column",
             width: DeviceWidth*0.3,
-            height: DeviceWidth*0.85,
+            //height: DeviceWidth*0.85,
             alignItems:'center',
             transform: [{scale: 0.8}],
             justifyContent: 'center'
